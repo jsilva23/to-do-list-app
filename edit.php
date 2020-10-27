2c0a2c3685edfc99d352378f10767ec44f3b430b
José da Silva <josesilva23p@gmai..com>
2020-10-27 14:58:15 +0100

    Validate edit form

--- edit.php
+++ edit.php
@@ -7,12 +7,20 @@ require 'helpers.php';
 
 $show_table = false;
 
-if (array_key_exists('name', $_POST) && $_POST['name'] != '') {
+$isThereError = false;
+$validationErrors = [];
+
+if (isTherePost()) {
     $task = [];
 
     $task['id'] = $_POST['id'];
 
-    $task['name'] = $_POST['name'];
+    if (array_key_exists('name', $_POST) && strlen($_POST['name']) > 0) {
+        $task['name'] = $_POST['name'];   
+    } else {
+        $isThereError = true;
+        $validationErrors['name'] = 'The task is necessary';        
+    }
 
     if (array_key_exists('description', $_POST)) {
         $task['description'] = $_POST['description'];
@@ -20,8 +28,13 @@ if (array_key_exists('name', $_POST) && $_POST['name'] != '') {
         $task['description'] = '';
     }
 
-    if (array_key_exists('term', $_POST)) {
-        $task['term'] = convertDateToDatabase($_POST['term']);
+    if (array_key_exists('term', $_POST) && strlen($_POST['term']) > 0) {
+        if (validateDate($_POST['term']) > 0) {
+            $task['term'] = convertDateToDatabase($_POST['term']);   
+        } else {
+            $isThereError = true;
+            $validationErrors['term'] = 'Deadline is not a valid date';
+        }
     } else {
         $task['term'] = '';
     }
@@ -34,10 +47,19 @@ if (array_key_exists('name', $_POST) && $_POST['name'] != '') {
         $task['completed'] = 0;
     }
 
-    editTask($conection, $task);
+    if (!$isThereError) {
+        editTask($conection, $task);
+        header('Location: todo.php');
+        die();   
+    }
 
 }
 
 $tasksList = searchTask($conection, $_POST['id']);
 
+$tasksList['name'] = (array_key_exists('name', $_POST)) ? $_POST['name'] : $tasksList['name'];
+$tasksList['description'] = (array_key_exists('description', $_POST)) ? $_POST['description'] : $tasksList['description'];
+$tasksList['term'] = (array_key_exists('term', $_POST)) ? $_POST['term'] : $tasksList['term'];
+$tasksList['priority'] = (array_key_exists('priority', $_POST)) ? $_POST['priority'] : $tasksList['priority'];
+$tasksList['completed'] = (array_key_exists('completed', $_POST)) ? $_POST['completed'] : $tasksList['completed'];
 require "template.php";
